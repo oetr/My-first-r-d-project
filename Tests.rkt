36aa447daf08cb4f914598785e68e217eb37c65c
Petr Samarin <petr@ps.fritz.box>
2010-12-13 16:27:57 +0100

    Added some new tests for all of the new functions...move! turn-left!, turn-right!, world builder, placement of the agent (random and manual).

--- Tests.rkt
+++ Tests.rkt
@@ -19,7 +19,7 @@
                                    (number->string i))))))
 
 (test-case
- "Color of the tiles in range"
+ "Color of the ground tiles is close to green"
  (let* ([test-name  "Color of the tiles in range "]
         [world-size 10]
         [world (build-environment world-size)])
@@ -33,6 +33,7 @@
         (check-true (< (color-b (tile-color (vector-ref world i)))
                        20) (string-append "B " test-name
                                           (number->string i))))))
+
 (test-case
  "Boundaries should not be movable"
  (let* ([test-name "Boundaries should not be movable (3x3) "]
@@ -46,7 +47,6 @@
                           'movable?) (string-append test-name ", test: 3"))
    (check-false (hash-ref (tile-object-on-top (vector-ref world 3))
                           'movable?) (string-append test-name ", test: 4")))
- "Boundaries should not be movable (7x7)"
  (let* ([test-name "Boundaries should not be movable (7x7) "]
         [world-size 7]
         [world (build-environment world-size)])
@@ -62,6 +62,154 @@
    (check-true (hash? (tile-object-on-top (vector-ref world 41))))
    (check-false (hash-ref (tile-object-on-top (vector-ref world 41))
                           'movable?) (string-append test-name ", test: 4"))))
+
+(test-case
+ "Random placement: Agent is not placed on occupied space "
+ (let* ([test-name "Random placement: Agent is not placed on occupied space "]
+        [world-size 4]
+        [environment (build-environment world-size)]
+        [agent (make-agent 0 0 0 0)])
+   (for ([i (in-range 20)])
+        (set! agent (place-agent-randomly agent environment world-size))
+        ;; (printf "agent: ~a~n" agent)
+        ;; (print-environment agent environment world-size)
+        (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                     (string-append test-name (number->string i))))))
+
+
+(test-case
+ "Manual placement: Agent is not placed on occupied space "
+ (let* ([test-name "Manual placement: Agent is not placed on occupied space "]
+        [world-size 5]
+        [environment (build-environment world-size)]
+        [agent (make-agent 0 0 0 0)])
+   ;; Placing the agent at 7
+   (set! agent (place-agent agent environment 7 0))
+   (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                (string-append test-name ": test " (number->string 1)))
+   (check-equal? 7 (agent-position agent) (string-append test-name ": test " (number->string 1)))                                                         
+   ;; Placing the agent at 8
+   (set! agent (place-agent agent environment 8 1))
+   (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                (string-append test-name ":test " (number->string 2)))
+   (check-equal? 8 (agent-position agent) (string-append test-name ":test " (number->string 2)))
+   ;; Placing the agent at 9
+   (set! agent (place-agent agent environment 9 2))
+   (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                (string-append test-name ":test " (number->string 3)))
+   (check-equal? 8 (agent-position agent) (string-append test-name ":test " (number->string 3)))
+   ;; Placing the agent at 10
+   (set! agent (place-agent agent environment 10 3))
+   (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                (string-append test-name ":test " (number->string 4)))
+   (check-equal? 8 (agent-position agent) (string-append test-name ":test " (number->string 4)))
+   ;; Placing the agent at 20
+   (set! agent (place-agent agent environment 20 0))
+   (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                (string-append test-name ":test " (number->string 5)))
+   (check-equal? 8 (agent-position agent) (string-append test-name ":test " (number->string 5)))
+   ;; Placing the agent at 17
+   (set! agent (place-agent agent environment 17 1))
+   (check-false (tile-object-on-top (vector-ref environment (agent-position agent)))
+                (string-append test-name ":test " (number->string 6)))
+   (check-equal? 17 (agent-position agent) (string-append test-name ":test " (number->string 6)))))
+
+(test-case
+ "Agent turns left"
+ ;; World with size 4
+ (let* ([test-name "Agent turns left "]
+        [world-size 4]
+        [environment (build-environment world-size)]
+        [agent (make-agent 0 0 0 0)])
+   (set! agent (place-agent agent environment 6 0))
+   ;; Turn left
+   (turn-left! agent environment)
+   (check-equal? (agent-position agent) 6 (string-append test-name ":test " (number->string 1)))
+   (check-equal? (agent-orientation agent) 3 (string-append test-name ":test " (number->string 1)))
+   ;; Another turn left
+   (turn-left! agent environment)
+   (check-equal? 6 (agent-position agent) (string-append test-name ":test " (number->string 2)))
+   (check-equal? 2 (agent-orientation agent) (string-append test-name ":test " (number->string 2)))
+   ;; Another turn left
+   (turn-left! agent environment)
+   (check-equal? 6 (agent-position agent) (string-append test-name ":test " (number->string 3)))
+   (check-equal? 1 (agent-orientation agent) (string-append test-name ":test " (number->string 3)))
+   ;; Another turn left
+   (turn-left! agent environment)
+   (check-equal? 6 (agent-position agent) (string-append test-name ":test " (number->string 4)))
+   (check-equal? 0 (agent-orientation agent) (string-append test-name ":test " (number->string 4)))
+   ;; Another turn left
+   (turn-left! agent environment)
+   (check-equal? 6 (agent-position agent) (string-append test-name ":test " (number->string 5)))
+   (check-equal? 3 (agent-orientation agent) (string-append test-name ":test " (number->string 5)))   
+   ))
+
+(test-case
+ "Agent turns right"
+ ;; World with size 5
+ (let* ([test-name "Agent turns right "]
+        [world-size 5]
+        [environment (build-environment world-size)]
+        [agent (make-agent 0 0 0 0)])
+   (set! agent (place-agent agent environment 13 0))
+   ;; Turn right
+   (turn-right! agent environment)
+   (check-equal? 13 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 1 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ;; Another turn right
+   (turn-right! agent environment)
+   (check-equal? 13 (agent-position agent) (string-append test-name ":test " (number->string 2)))
+   (check-equal? 2 (agent-orientation agent) (string-append test-name ":test " (number->string 2)))
+   ;; Another turn right
+   (turn-right! agent environment)
+   (check-equal? 13 (agent-position agent) (string-append test-name ":test " (number->string 3)))
+   (check-equal? 3 (agent-orientation agent) (string-append test-name ":test " (number->string 3)))
+   ;; Another turn right
+   (turn-right! agent environment)
+   (check-equal? 13 (agent-position agent) (string-append test-name ":test " (number->string 4)))
+   (check-equal? 0 (agent-orientation agent) (string-append test-name ":test " (number->string 4)))
+   ;; Turn right
+   (turn-right! agent environment)
+   (check-equal? 13 (agent-position agent) (string-append test-name ":test " (number->string 5)))
+   (check-equal? 1 (agent-orientation agent) (string-append test-name ":test " (number->string 5)))   
+   ))
+
+(test-case
+ "Moving around"
+ ;; World with size 5
+ (let* ([test-name "Moving around "]
+        [world-size 5]
+        [environment (build-environment world-size)]
+        [agent (make-agent 0 0 0 0)])
+   (set! agent (place-agent agent environment 13 3))
+   ;; Move
+   (move! agent environment)
+   (check-equal? 12 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 3 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ;; Move
+   (move! agent environment)
+   (check-equal? 11 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 3 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ;; Move
+   (move! agent environment)
+   (check-equal? 11 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 3 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ;; Setting the agent to another position
+   (set! agent (place-agent agent environment 18 0))
+   ;; Move
+   (move! agent environment)
+   (check-equal? 13 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 0 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ;; Move
+   (move! agent environment)
+   (check-equal? 8 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 0 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ;; Move
+   (move! agent environment)
+   (check-equal? 8 (agent-position agent) (string-append test-name ":test " (number->string 1)))
+   (check-equal? 0 (agent-orientation agent) (string-append test-name ":test " (number->string 1)))
+   ))
+
 
 (test-case
  "Testing position->cordinates"
